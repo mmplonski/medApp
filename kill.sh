2f55a2c314e922b4b8252fd82c4aebd05dcdd35f
mmplonski <mplonski20@wp.pl>
2023-04-17 23:04:08 +0200

    Init

---
+++ kill.sh
@@ -0,0 +1,5 @@
+lsof -ti :8080 | xargs kill
+lsof -ti :9000 | xargs kill
+lsof -ti :8088 | xargs kill
+lsof -ti :8087 | xargs kill
+lsof -ti :8761 | xargs kill
---
+++ start.sh
@@ -0,0 +1,5 @@
+java -jar eureka.jar &
+java -jar gateway.jar &
+java -jar patientsService.jar &
+java -jar referralForMedicalTestService.jar &
+java -jar researchProjectsService.jar &
